3c18f337ed2471542542893351d8546341128640
Lomias0531 <460492314@qq.com>
2020-07-09 11:53:19 +0800

    Merge

---
+++ Elysium_Project/Elysium_Project/Scripts/DataManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium_Project.Scripts
+{
+    class DataManager:Singletion<DataManager>
+    {
+        public Config config;
+        public Galaxy_Map galaxy;
+        public Civilization civ;
+    }
+    public class Config
+    {
+
+    }
+    public class Galaxy_Map
+    {
+
+    }
+    public class Civilization
+    {
+
+    }
+}
---
+++ Elysium_Project/Elysium_Project/Scripts/Singletion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium_Project.Scripts
+{
+    public class Singletion<T> where T: new()
+    {
+        private static T _T;
+        public static T instance()
+        {
+            if(_T == null)
+            {
+                _T = new T();
+            }
+            return _T;
+        }
+        public virtual void init() { }
+    }
+}
