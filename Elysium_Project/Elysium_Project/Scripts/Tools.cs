12a25d27938c2683cdd050499041cce9ef858087
Lomias0531 <460492314@qq.com>
2020-07-15 09:10:06 +0800

    EEE

---
+++ Elysium_Project/Elysium_Project/Scripts/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium_Project.Scripts
+{
+    class Tools:Singletion<Tools>
+    {
+        public string GetRandName(int num)
+        {
+            return null;
+        }
+    }
+}
